cc2b6afcef1134a7e03899077d94369159262bbe
iiglesiasg <ningunpenk@hotmail.com>
2020-05-28 21:04:56 +0200

    implicit helm repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/iiglesiasg/elastic-pulumi.git && cd elastic-pulumi
 npm install
 helm init --client-only
-PULUMI_ACCESS_TOKEN=pul-c0f5b1da538fe7b321598b9fc1e012546680e54c
+export PULUMI_ACCESS_TOKEN=pul-c0f5b1da538fe7b321598b9fc1e012546680e54c
 echo $KUBECONFIG
 /root/.pulumi/bin/pulumi login
 #/root/.pulumi/bin/pulumi stack select elastic 
