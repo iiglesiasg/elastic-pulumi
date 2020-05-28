988a502c8a8407ad9b7fdc97b82741e43538c9eb
iiglesiasg <iiglesig@everis.com>
2020-05-28 14:36:00 +0200

    pulumi access token

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ git clone https://github.com/iiglesiasg/elastic-pulumi.git
 cd elastic-pulumi
 pwd
 echo $PATH
+export PULUMI_ACCESS_TOKEN=pul-c0f5b1da538fe7b321598b9fc1e012546680e54c
 /root/.pulumi/bin/pulumi login
 /root/.pulumi/bin/pulumi preview
 tail -f /dev/null
